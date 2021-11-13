01367906e741d8c0179d414e7a22158b0206e646
krish <krishdu.p@gmail.com>
2021-11-13 13:33:37 +0530

    Initial upload with a minimal UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="help text-primary h5">
+        Pick 1 or more numbers that sum to the number of candies
+      </div>
+      <div className="body">
+        <div className="left">
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+          <div className="candy" />
+        </div>
+        <div className="right">
+          <button className="number">1</button>
+          <button className="number">2</button>
+          <button className="number">3</button>
+          <button className="number">4</button>
+          <button className="number">5</button>
+          <button className="number">6</button>
+          <button className="number">7</button>
+          <button className="number">8</button>
+          <button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer h5">Time Remaining: 10</div>
     </div>
   );
 }
